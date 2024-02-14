5d8f3c7fc63aeb08420a600378ff9e43a36558b4
sugamsubedi3 <157234324+sugamsubedi3@users.noreply.github.com>
2024-02-13 21:20:51 -0500

    Add files via upload
    this is the in class exercise that you give.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿def calculate_times_table():
+    while True:
+        try:
+            n = int(input("Enter a number below 10: "))
+            if 1 <= n < 10:
+                for i in range(1, 10):
+                    print(f"{i} * {n} = {i * n}")
+                break
+            else:
+                print("Please enter a number below 10.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+calculate_times_table()
